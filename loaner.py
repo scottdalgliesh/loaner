a632fd52c3d0151d9df9e0d45c6e2d3b235b1535
Scott Dalgliesh <scott.t.dalgliesh@gmail.com>
2020-11-01 00:47:05 -0400

    rewritten as class

--- loaner.py
+++ loaner.py
@@ -1,62 +1,125 @@
-#pylint: disable=[invalid-name]
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from datetime import date
+from typing import Optional, Tuple
 
 import pandas as pd
+from dateutil.relativedelta import relativedelta
 
 # assumptions:
 # - compound interest
 # - monthly payment / compunding
 
-# inputs
-princ = 10_000              # principal
-inter = 0.05                # interest rate (% annual)
-payme = 250                 # monthly payment
-d, m, y = (1, 1, 2020)      # first repayment date (DD,MM,YYYY)
 
-# initialize (first month)
-ind = 0
-date = [datetime(year=y, month=m, day=d)]       # Payment date
-bal_open = [princ]                              # monthly opening balance
-int_acc = [bal_open[ind]*inter/12]              # interest accrued during month
-contrib = [payme]                               # contribution (payment)
-bal_close = [bal_open[0] + int_acc[0] - contrib[0]]
+class Loan:
+    """Creates a Loan object. Used for assessment of loan repayment options.
+    """
 
-# monthly payments
-while bal_close[ind] > 0:
-    ind += 1
-    date.append(date[ind-1] + relativedelta(months=+1))
-    bal_open.append(bal_close[ind-1])
-    int_acc.append(bal_open[ind]*inter/12)
-    contrib.append(payme)
-    if bal_open[ind] + int_acc[ind] < contrib[ind]:
-        contrib[ind] = bal_open[ind] + int_acc[ind]
-    bal_close.append(bal_open[ind] + int_acc[ind] - contrib[ind])
+    def __init__(self, princ: float, inter: float,
+                 payme: float, start: Optional[Tuple[int]] = None) -> None:
+        if princ <= 0:
+            raise ValueError("Principal must be greater than zero.")
+        self.princ = princ
 
-# repayment summary table
-pd.options.display.float_format = "${:,.2f}".format
-sum_df = pd.DataFrame(
-    {
-        "Date": date,
-        "Opening Balance": bal_open,
-        "Accrued Interest": int_acc,
-        "Contribution": contrib,
-        "Closing Balance": bal_close
-    }
-)
-tot_int = sum_df['Accrued Interest'].sum()
-tot_pay = sum_df["Contribution"].sum()
+        if not 0 < inter < 1:
+            raise ValueError("Interest must be between 0 and 1.")
+        self.inter = inter
 
-# output results
-out = (f"Loan Summary\n"
-       "------------\n"
-       f"Principal:         ${princ:.2f}\n"
-       f"Interest rate:     {inter:.2f}%\n"
-       f"Monthly Payment:   ${payme:.2f}\n"
-       f"Repayment Period:  {len(sum_df)} weeks\n"
-       f"Total interest:    ${tot_int:.2f}\n"
-       f"Total paid:        ${tot_pay:.2f}\n\n\n"
-       f"Repayment summary table:\n"
-       "------------------------\n"
-       f"{sum_df}")
-print(out)
+        if payme <= 0:
+            raise ValueError("Payment must be greater than 0.")
+        self.payme = payme
+
+        if start is None:
+            self.start = date.today()
+        elif self._check_start_date(start):
+            self.start = date(year=start[2], month=start[1], day=start[0])
+        else:
+            raise ValueError("Date must be specified as (mm,dd,yyyy).")
+
+        pd.options.display.float_format = "${:,.2f}".format
+        pd.options.display.width = 0
+        self.table = self._calculate_payment_schedule()
+        self.tot_int = round(self.table['Accrued Interest'].sum(), 2)
+        self.tot_pay = round(self.table["Contribution"].sum(), 2)
+
+    def _calculate_payment_schedule(self) -> pd.DataFrame:
+        """Calculate a payment summary table for the loan.
+
+        Returns
+        -------
+        pd.DataFrame
+            Payment summary table stored in a DataFrame
+        """
+
+        # initialize (first month)
+        ind = 0
+        pay_date = [self.start]
+        bal_open = [self.princ]
+        int_acc = [bal_open[ind]*self.inter/12]
+        contrib = [self.payme]
+        bal_close = [bal_open[0] + int_acc[0] - contrib[0]]
+
+        # monthly payments
+        while bal_close[ind] > 0:
+            ind += 1
+            pay_date.append(pay_date[ind-1] + relativedelta(months=+1))
+            bal_open.append(bal_close[ind-1])
+            int_acc.append(bal_open[ind]*self.inter/12)
+            contrib.append(self.payme)
+            if bal_open[ind] + int_acc[ind] < contrib[ind]:
+                contrib[ind] = bal_open[ind] + int_acc[ind]
+            bal_close.append(bal_open[ind] + int_acc[ind] - contrib[ind])
+
+        sum_df = pd.DataFrame(
+            {
+                "Payment Date": pay_date,
+                "Opening Balance": bal_open,
+                "Accrued Interest": int_acc,
+                "Contribution": contrib,
+                "Closing Balance": bal_close
+            }
+        )
+        return sum_df
+
+    @staticmethod
+    def _check_start_date(start) -> bool:
+        """Validate start date input.
+
+        Parameters
+        ----------
+        start : Tuple
+            Start date entered as (mm,dd,yyyy)
+
+        Returns
+        -------
+        bool
+        """
+
+        if not len(start) == 3:
+            return False
+        try:
+            date(year=start[2], month=start[1], day=start[0])
+        except ValueError:
+            return False
+        return True
+
+    def __repr__(self):
+        """convert to formal string, for repr()."""
+        date_str = self.start.strftime("%m,%d,%Y")
+        msg = (f"Loan(princ={self.princ}, inter={self.inter}"
+               f", payme={self.payme}, start=({date_str}))")
+        return msg
+
+    def __str__(self):
+        """convert to string representation"""
+        msg = ("Loan Summary\n"
+               "------------\n"
+               f"Principal:         ${self.princ:.2f}\n"
+               f"Interest rate:     {self.inter:.2f}%\n"
+               f"Monthly Payment:   ${self.payme:.2f}\n"
+               f"Start Date:        {self.start.strftime('%m,%d,%Y')}\n"
+               f"Repayment Period:  {len(self.table)} weeks\n"
+               f"Total interest:    ${self.tot_int:.2f}\n"
+               f"Total paid:        ${self.tot_pay:.2f}\n\n\n"
+               f"Repayment summary table:\n"
+               "------------------------\n"
+               f"{self.table}")
+        return msg
